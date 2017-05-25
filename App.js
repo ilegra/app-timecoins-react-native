ab8b48b4c6ceadb8a4a31ae632f1172f3a4a0915
Maíra de Oliveira <mairasetecores@gmail.com>
2017-05-25 15:09:12 -0300

    creating header

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header.js';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
+        <Header headerText={'timeCoins'} />
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
@@ -19,5 +21,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
