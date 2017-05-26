eac6dc7be370591c1b43e98744d3730b5c08c693
Maíra de Oliveira <mairasetecores@gmail.com>
2017-05-26 16:29:59 -0300

    add logo

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import { View } from 'react-native';
-import Header from './src/components/commons/Header';
+import { View, Image, ScrollView } from 'react-native';
+import Header  from './src/components/commons/Header';
 import InputForm from './src/components/InputForm';
 
 export default class App extends React.Component {
   render() {
     return (
       <View>
-        <Header headerText={'timeIntoCoins'} />
-        <InputForm />
+          <Header headerText={'timeIntoCoins'} />
+          <ScrollView>
+            <Image
+              style={styles.imageStyle}
+              source={require('./assets/images/logo.png')}
+            />
+            <InputForm />
+          </ScrollView>
       </View>
     );
   }
 }
+
+const styles = {
+  imageStyle: {
+    width: 300,
+    height: 300,
+    alignItems: 'center'
+  },
+};
