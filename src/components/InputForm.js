800204758c61c331a62a8ac9281e13d1cd290e44
Maíra de Oliveira <mairasetecores@gmail.com>
2017-05-29 10:34:44 -0300

    InputForm created

---
+++ src/components/InputForm.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Card, CardSection, Input, Button } from './common';
+
+class InputForm extends Component {
+
+  render() {
+    return (
+        <Card>
+          <CardSection>
+            <Input
+              placeholder="Enter the average salary of participants"
+            />
+          </CardSection>
+
+          <CardSection>
+            <Input
+              placeholder="Enter the number of participants"
+            />
+          </CardSection>
+
+          <CardSection>
+              <Button>
+                start
+              </Button>
+          </CardSection>
+        </Card>
+    );
+  }
+}
+
+export default InputForm;
---
+++ src/components/common/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// Make a component
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#6F3AAD',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 80,
+    paddingTop: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    color: '#E6E6E6',
+    fontSize: 20,
+    paddingTop: 20
+  }
+};
+
+export default Header;
