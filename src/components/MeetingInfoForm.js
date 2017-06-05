5afd48b74c19da84276fb987ee6011ef84d35907
Maíra de Oliveira <mairasetecores@gmail.com>
2017-06-05 16:44:56 -0300

    modifing MeetingInfoForm

--- src/components/MeetingInfoForm.js
+++ src/components/MeetingInfoForm.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { Card, CardSection, Input, Button } from './common';
 
+
 class MeetingInfoForm extends Component {
+  state = { salary: '', number: '' };
+
+  onButtonPress() {
+  }
 
   render() {
     return (
@@ -9,16 +14,20 @@ class MeetingInfoForm extends Component {
           <CardSection>
             <Input
               placeholder="Enter the average salary of participants"
+              value={this.state.salary}
+              onChangeText={salary => this.setState({ salary })}
             />
           </CardSection>
 
           <CardSection>
             <Input
               placeholder="Enter the number of participants"
+              value={this.state.number}
+              onChangeText={number => this.setState({ number })}
             />
           </CardSection>
 
-            <Button>
+            <Button onPress={this.onButtonPress.bind(this)}>
               {require('../../assets/images/botao-play.png')}
             </Button>
         </Card>
