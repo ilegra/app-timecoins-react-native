cee765cfa59e47c2017a299b9a3c4692a619e431
Maíra de Oliveira <mairasetecores@gmail.com>
2017-06-07 17:03:31 -0300

    resultPage in process

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -8,10 +8,10 @@ class ResultPage extends Component {
        <Card>
 
         <Text>Total meeting time was:</Text>
-          {this.props.currentTime}
+        
 
         <Text>The meeting cost to your company:</Text>
-          {this.props.currentTime}
+
 
         <Button onPress={this.props.onReset}>
           {require('../../assets/images/botao-recomeçar.png')}
