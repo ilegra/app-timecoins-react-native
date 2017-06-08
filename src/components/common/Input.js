58144a79ce0e2b9533f4f9a7e60d727b00d8efdf
Maíra de Oliveira <mairasetecores@gmail.com>
2017-06-08 12:35:27 -0300

    inserted numeric keyboardType

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -7,6 +7,7 @@ const Input = ({ value, onChangeText, placeholder }) => {
   return (
     <View style={containerStyle}>
       <TextInput
+        keyboardType={'numeric'}
         placeholder={placeholder}
         autoCorrect={false}
         style={inputStyle}
