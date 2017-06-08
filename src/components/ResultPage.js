30a17350666a1cb41d1d97c5d6c022e1530c7f92
Maíra de Oliveira <mairasetecores@gmail.com>
2017-06-08 13:06:26 -0300

    modified text

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { Card, Button } from './common';
 
 class ResultPage extends Component {
    render() {
      return (
        <Card>
+        <View style={styles.container}>
 
-        <Text>Total meeting time was:</Text>
-        
-
-        <Text>The meeting cost to your company:</Text>
+            <Text style={styles.text}>Total meeting time was:</Text>
+            <Text style={styles.text}>The meeting cost to your company:</Text>
 
 
-        <Button onPress={this.props.onReset}>
-          {require('../../assets/images/botao-recomeçar.png')}
-        </Button>
+          <Button onPress={this.props.onReset}>
+            {require('../../assets/images/botao-recomeçar.png')}
+          </Button>
+        </View>
        </Card>
      );
    }
 }
+
+const styles = {
+  container: {
+    padding: 5,
+    borderRadius: 5,
+    alignItems: 'center'
+
+  },
+  text: {
+    fontSize: 17,
+    color: '#4D4D4D',
+    marginLeft: 7,
+  }
+};
 
 export default ResultPage;
