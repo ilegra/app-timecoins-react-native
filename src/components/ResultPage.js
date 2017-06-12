5259eaaa4f3db88654804602f94b7826e7b10148
Maíra de Oliveira <mairasetecores@gmail.com>
2017-06-12 18:22:57 -0300

    adding style

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -8,9 +8,29 @@ class ResultPage extends Component {
        <Card>
         <View style={styles.container}>
 
-            <Text style={styles.text}>Total meeting time was:</Text>
-            <Text style={styles.text}>The meeting cost to your company:</Text>
+              <Text style={styles.text}>
+                Total meeting time was:
+              </Text>
 
+              <View
+                style={[styles.firstBackgroundColor, styles.containerResults]}
+              >
+                <Text style={styles.textTime}>
+                  00:00:00
+                </Text>
+              </View>
+
+              <Text style={styles.text}>
+                The meeting cost to your company:
+              </Text>
+
+              <View
+                style={[styles.secondBackgroundColor, styles.containerResults]}
+              >
+                <Text style={styles.textCost}>
+                  $387,78
+                </Text>
+              </View>
 
           <Button onPress={this.props.onReset}>
             {require('../../assets/images/botao-recomeçar.png')}
@@ -25,13 +45,37 @@ const styles = {
   container: {
     padding: 5,
     borderRadius: 5,
-    alignItems: 'center'
-
+    alignItems: 'center',
   },
   text: {
     fontSize: 17,
     color: '#4D4D4D',
-    marginLeft: 7,
+    marginBottom: 10,
+    marginTop: 25
+  },
+  containerResults: {
+    flex: 2,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    marginBottom: 25
+  },
+  firstBackgroundColor: {
+    backgroundColor: '#FFC543'
+  },
+  secondBackgroundColor: {
+    backgroundColor: '#6F3AAD'
+  },
+  textTime: {
+    fontSize: 30,
+    color: '#6F3AAD',
+    marginTop: 20,
+    marginBottom: 20
+  },
+  textCost: {
+    fontSize: 30,
+    color: '#FFC543',
+    marginTop: 20,
+    marginBottom: 20
   }
 };
 
