a1052f97e8d02171f6e9f1e5ebdc012d3c6aa978
Maíra de Oliveira <mairasetecores@gmail.com>
2017-06-13 12:08:17 -0300

    modified space between elements

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -82,7 +82,7 @@ const styles = {
     flex: 2,
     alignSelf: 'stretch',
     alignItems: 'center',
-    marginBottom: 25
+    marginBottom: 40
   },
   firstBackgroundColor: {
     backgroundColor: '#FFC543'
